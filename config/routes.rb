e1f0175661419e9c3e5946f511e21351912612aa
Guilherme <guigfaria01@gmail.com>
2022-11-01 14:28:27 -0300

    fix profile to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-
-  namespace :profiles_backoffice do
+  
+  namespace :users_backoffice do
     get 'welcome/index'
   end
-
+  
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-
+  
+  devise_for :users
   devise_for :admins
-  devise_for :profiles
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
