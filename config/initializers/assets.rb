815dd247a433d7db48c4cfd24fdd7596a647ef47
Guilherme <guigfaria01@gmail.com>
2022-11-03 15:40:18 -0300

    fix logout link into layouts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,7 +20,7 @@ Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_ba
 # lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
                                                   custom.js     custom.css
-                                                  img.jpg                      )
+                                                  img.png                      )
 
 # vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
