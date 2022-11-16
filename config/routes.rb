72c0544fc5b38927cd1fbb56fbf928d16a0856c7
Guilherme <guigfaria01@gmail.com>
2022-11-16 16:06:24 -0300

    add name to user and cleanup user backoffice layout

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
