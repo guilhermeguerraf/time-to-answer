77f71de6a1c9d63cbe192905d6e433d906a8a7ab
Guilherme <guigfaria01@gmail.com>
2022-11-22 16:05:56 -0300

    add user statistics

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,5 +1,9 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer])
+
+    if user_signed_in?
+      UserStatistic.set_statistics(@answer, current_user)
+    end
   end
 end
---
+++ app/models/user_statistic.rb
@@ -0,0 +1,13 @@
+class UserStatistic < ApplicationRecord
+  belongs_to :user
+
+  def total_questions
+    self.right_questions + self.wrong_questions
+  end
+
+  def self.set_statistics(answer, current_user)
+    user_statistic = UserStatistic.find_or_create_by(user: current_user)
+    answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+    user_statistic.save!
+  end
+end
