46b24daf0c4153b9ffc17afeb63412a047d6c00b
Guilherme <guigfaria01@gmail.com>
2022-11-29 18:01:15 -0300

    add subjects section into site/welcome/index

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,5 +1,6 @@
 class Site::WelcomeController < SiteController
   def index
     @questions = Question.includes(:answers, :subject).order(created_at: :desc).page(params[:page]).per(5)
+    @subjects = Subject.order(questions_count: :desc).limit(6)
   end
 end
